95033459ce8618de3562bedc7124efa8569702d9
Gabriel Mello <gabrielrmello515@gmail.com>
2025-04-30 22:18:36 -0300

    feat new routes and params

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,10 +1,12 @@
 import 'package:geekcontrol/animes/articles/pages/articles_page.dart';
+import 'package:geekcontrol/animes/season/season_releases.dart';
 import 'package:geekcontrol/animes/spoilers/pages/spoilers_page.dart';
-import 'package:geekcontrol/animes/ui/pages/details_anime_page.dart';
-import 'package:geekcontrol/animes/ui/pages/releases_animes_page.dart';
+import 'package:geekcontrol/animes/ui/pages/details_page.dart';
+import 'package:geekcontrol/animes/ui/pages/latest_releases_page.dart';
+import 'package:geekcontrol/animes/ui/pages/top_rateds_page.dart';
 import 'package:geekcontrol/home/pages/home_page.dart';
-import 'package:geekcontrol/screen_test.dart';
 import 'package:geekcontrol/services/sites/otakupt/profile.dart';
+import 'package:geekcontrol/services/sites/wallpapers/pages/wallpapers_fullscreen_page.dart';
 import 'package:geekcontrol/services/sites/wallpapers/pages/wallpapers_page.dart';
 import 'package:geekcontrol/settings_page/pages/settings_page.dart';
 import 'package:go_router/go_router.dart';
@@ -23,11 +25,11 @@ class AppRoutes {
       ),
       GoRoute(
         path: '/releases',
-        builder: (context, state) => const ReleasesAnimesPage(),
+        builder: (context, state) => const LatestReleasesPage(),
       ),
       GoRoute(
         path: '/test',
-        builder: (context, state) => const TestScreen(),
+        builder: (context, state) => const SeasonReleasesPage(season: 'Test'),
       ),
       GoRoute(
         path: '/profile',
@@ -35,7 +37,10 @@ class AppRoutes {
       ),
       GoRoute(
         path: '/details',
-        builder: (context, state) => const DetailsAnimePage(),
+        builder: (context, state) {
+          final id = state.extra as int;
+          return DetailsPage(id: id);
+        },
       ),
       GoRoute(
         path: '/spoilers',
@@ -46,8 +51,29 @@ class AppRoutes {
         builder: (context, state) => const WallpaperListScreen(),
       ),
       GoRoute(
+        path: '/wallpaper-fullscreen',
+        builder: (context, state) {
+          final data = state.extra as Map;
+          return WallpaperFullscreen(
+              images: data['images'], index: data['index']);
+        },
+      ),
+      GoRoute(
         path: '/settings',
         builder: (context, state) => const SettingsPage(),
+      ),
+      GoRoute(
+        path: '/top-rateds',
+        builder: (context, state) => const TopRatedsPage(),
+      ),
+      GoRoute(
+        path: '/season-releases',
+        builder: (context, state) {
+          final data = state.extra as String;
+          return SeasonReleasesPage(
+            season: data,
+          );
+        },
       ),
     ],
   );
