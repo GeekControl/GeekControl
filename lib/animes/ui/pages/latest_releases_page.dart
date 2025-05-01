b05f35d84f4e0ab26db68b14c558bf980574f571
Gabriel Mello <gabrielrmello515@gmail.com>
2025-04-30 22:17:00 -0300

    feat latest releases page

---
+++ lib/animes/ui/pages/latest_releases_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:geekcontrol/animes/components/fields_component.dart';
+import 'package:geekcontrol/animes/ui/pages/details_page.dart';
+import 'package:geekcontrol/core/utils/skeletonizer/cards_skeletonizer.dart';
+import 'package:geekcontrol/home/pages/home_page.dart';
+import 'package:geekcontrol/services/anilist/controller/anilist_controller.dart';
+import 'package:geekcontrol/services/anilist/entities/releases_anilist_entity.dart';
+import 'package:go_router/go_router.dart';
+
+class LatestReleasesPage extends StatefulWidget {
+  static const route = '/releases';
+  const LatestReleasesPage({super.key});
+
+  @override
+  State<LatestReleasesPage> createState() => _LatestReleasesPageState();
+}
+
+class _LatestReleasesPageState extends State<LatestReleasesPage> {
+  final _ct = AnilistController();
+  late Future<List<ReleasesAnilistEntity>> _future;
+  final List<int> readers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _future = _ct.getReleasesAnimes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => GoRouter.of(context).pushReplacement(HomePage.route),
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Center(child: Text('Últimos lançamentos')),
+      ),
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CardsSkeletonizer();
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                ReleasesAnilistEntity releases = snapshot.data![index];
+                return GestureDetector(
+                  onTap: () => GoRouter.of(context)
+                      .push(DetailsPage.route, extra: releases.id),
+                  child: Card(
+                    margin: const EdgeInsets.all(8),
+                    elevation: 4,
+                    color: readers.contains(releases.id)
+                        ? const Color.fromARGB(255, 169, 191, 209)
+                        : Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Hero(
+                          tag: releases.bannerImage,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                            child: Image.network(
+                              releases.bannerImage,
+                              height: 130,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(6),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Text(
+                                releases.englishTitle,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            FieldsComponent(releases: releases),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
