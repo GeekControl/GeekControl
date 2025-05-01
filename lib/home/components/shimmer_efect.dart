d6734873d0d684877dfe7373a9edd0e0e7af5ee9
Gabriel Mello <gabrielrmello515@gmail.com>
2025-04-30 22:20:08 -0300

    feat shimmer effect

---
+++ lib/home/components/shimmer_efect.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:geekcontrol/core/library/hitagi_cup/features/containter/hitagi_container.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerEffect extends StatelessWidget {
+  final double height; 
+  final double width;
+  const ShimmerEffect({super.key,required this.height, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[900]!,
+      highlightColor: Theme.of(context).colorScheme.primary,
+      child: HitagiContainer(
+        color: Colors.grey[400],
+        height: height,
+        width: width,
+      ),
+    );
+  }
+}
