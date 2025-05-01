30e34c67f290b6d911675edbb332df89b1cd9f29
Gabriel Mello <gabrielrmello515@gmail.com>
2025-04-30 22:20:54 -0300

    feat minimalist and new home

--- lib/home/pages/home_page.dart
+++ lib/home/pages/home_page.dart
@@ -1,45 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:geekcontrol/animes/articles/pages/articles_page.dart';
+import 'package:geekcontrol/animes/season/season_releases.dart';
+import 'package:geekcontrol/core/library/hitagi_cup/features/containter/hitagi_card_container.dart';
 import 'package:geekcontrol/home/components/releases_carousel.dart';
 import 'package:geekcontrol/home/components/top_rateds_carousel.dart';
 import 'package:geekcontrol/core/library/hitagi_cup/features/text/hitagi_text.dart';
-import 'package:geekcontrol/home/atoms/bottom_bar.dart';
 import 'package:geekcontrol/home/components/banner_carrousel.dart';
 import 'package:geekcontrol/home/components/top_bar_widget.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
+  static const route = '/';
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: TopBarWidget(),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                left: 8,
-                bottom: 8,
-              ),
-              child: HitagiText(
-                text: 'Últimas notícias!',
-                typography: HitagiTypography.button,
-              ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const HitagiText(
+                        text: 'Últimas notícias',
+                        typography: HitagiTypography.button,
+                      ),
+                      IconButton(
+                        onPressed: () =>
+                            GoRouter.of(context).push(ArticlesPage.route),
+                        icon: const Icon(Icons.arrow_forward),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 150,
+                  child: BannerCarousel(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6.0),
+                  child: Column(
+                    children: [
+                      const ReleasesCarousel(),
+                      const TopRatedsCarousel(),
+                    ],
+                  ),
+                ),
+                HitagiCardContainer(
+                  title: 'Primavera 2025',
+                  subtitle: 'Novos animes chegaram!',
+                  description:
+                      'Confira os destaques e estreias mais esperadas desta primavera',
+                  route: SeasonReleasesPage.route,
+                ),
+              ],
             ),
-            SizedBox(
-              height: 150,
-              child: BannerCarousel(),
-            ),
-            ReleasesCarousel(),
-            TopRatedsCarousel(),
-          ],
-        ),
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomBarWidget(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
