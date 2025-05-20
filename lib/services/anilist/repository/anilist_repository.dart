83d1543ecc0dbe3b0ab690cce8d2ca2cf02c2a19
Gabriel Mello <gabrielrmello515@gmail.com>
2025-05-20 18:23:03 -0300

    fix: correct query parameters in getReleasesAnimes method and improve formatting

--- lib/services/anilist/repository/anilist_repository.dart
+++ lib/services/anilist/repository/anilist_repository.dart
@@ -10,8 +10,8 @@ import 'package:logger/logger.dart';
 class AnilistRepository {
   Future<List<MangasRates>> getRateds({required AnilistTypes type}) async {
     try {
-      final response = await AnilistUtils.basicResponse(
-          query: Query.ratedsQuery(type));
+      final response =
+          await AnilistUtils.basicResponse(query: Query.ratedsQuery(type));
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> jsonResponse = json.decode(response.body);
@@ -26,10 +26,11 @@ class AnilistRepository {
     }
   }
 
-  Future<List<ReleasesAnilistEntity>> getReleasesAnimes({required AnilistTypes type}) async {
+  Future<List<ReleasesAnilistEntity>> getReleasesAnimes(
+      {required AnilistTypes type, String? year}) async {
     try {
       final response = await AnilistUtils.basicResponse(
-        query: Query.releasesQuery('', type),
+        query: Query.releasesQuery(type, year: year),
       );
       if (response.statusCode == 200) {
         final Map<String, dynamic> jsonResponse = json.decode(response.body);
