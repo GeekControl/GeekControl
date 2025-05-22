fb1290cdf6b3d5671a08a1f5f645469deae84158
Gabriel Mello <gabrielrmello515@gmail.com>
2025-05-22 16:28:50 -0300

    fix: replace GetIt instance with di for ArticlesController registration

--- lib/view/animes/articles/pages/articles_module.dart
+++ lib/view/animes/articles/pages/articles_module.dart
@@ -1,12 +1,11 @@
 import 'package:geekcontrol/view/animes/articles/controller/articles_controller.dart';
 import 'package:geekcontrol/core/utils/global_variables.dart';
 import 'package:geekcontrol/core/utils/module_factory.dart';
-import 'package:get_it/get_it.dart';
 
 class ArticlesModule implements Module {
   @override
   void register() {
     Globals().initializeLogger(runtimeType);
-    GetIt.instance.registerFactory(() => ArticlesController());
+    di.registerFactory(() => ArticlesController());
   }
 }
