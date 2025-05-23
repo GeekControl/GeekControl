8b6d7b6216d1a029b86864b0f46d6ef2aacf5d8f
Gabriel Mello <gabrielrmello515@gmail.com>
2025-05-23 01:27:01 -0300

    feat: restructure AssetsEnum and BannerAssetsEnum for improved asset management

--- lib/core/utils/assets_enum.dart
+++ lib/core/utils/assets_enum.dart
@@ -4,13 +4,24 @@ enum AssetsEnum {
   otakuPtLogo('assets/otakuPT_icon.png'),
   intoxiLogo('assets/intoxi.png'),
   defaultImage('assets/default.png'),
+  geekcontrolLogo('assets/geekcontrol_logo.png');
+
+  final String path;
+  const AssetsEnum(this.path);
+}
+
+enum BannerAssetsEnum {
   luffy('assets/banners_assets/luffy.png'),
   hitagi('assets/banners_assets/hitagi.png'),
   berserker('assets/banners_assets/berserker.png'),
   frieren('assets/banners_assets/frieren.png'),
   zenitsu('assets/banners_assets/zenitsu.png'),
-  geekcontrolLogo('assets/geekcontrol_logo.png');
+  albedoWhite('assets/banners_assets/albedo_white.png'),
+  oshinoShinobuPink('assets/banners_assets/oshino_shinobu_pink.png'),
+  oshinoShinobuPurple('assets/banners_assets/oshino_shinobu_purple.png'),
+  soloLevelingPurple('assets/banners_assets/solo_leveling_purple.jpeg'),
+  shalltearWhite('assets/banners_assets/shalltear_white.png');
 
   final String path;
-  const AssetsEnum(this.path);
+  const BannerAssetsEnum(this.path);
 }
