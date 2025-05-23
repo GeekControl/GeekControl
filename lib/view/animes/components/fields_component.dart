c80f2fb870a89e02aded1868e9a9b425f8a7dc34
Gabriel Mello <gabrielrmello515@gmail.com>
2025-05-23 01:27:36 -0300

    feat: enhance FieldsComponent to support chapters and volumes based on AnilistTypes

--- lib/view/animes/components/fields_component.dart
+++ lib/view/animes/components/fields_component.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:geekcontrol/core/library/hitagi_cup/features/text/hitagi_text.dart';
 import 'package:geekcontrol/core/utils/date_time.dart';
+import 'package:geekcontrol/view/services/anilist/entities/anilist_types_enum.dart';
 import 'package:geekcontrol/view/services/anilist/entities/releases_anilist_entity.dart';
 
-
 class FieldsComponent extends StatelessWidget {
+  final AnilistTypes type;
   final ReleasesAnilistEntity releases;
 
-  const FieldsComponent({super.key, required this.releases});
+  const FieldsComponent(
+      {super.key, required this.releases, required this.type});
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +27,17 @@ class FieldsComponent extends StatelessWidget {
         ),
         const SizedBox(height: 4),
         HitagiText.icon(
-          'Episódios - ${releases.actuallyEpisode}/${releases.episodes}',
+          type == AnilistTypes.anime
+              ? 'Episódios - ${releases.actuallyEpisode}/${releases.episodes}'
+              : 'Capítulos - ${releases.chapters}',
           Icons.movie,
         ),
+        releases.volumes! > 0
+            ? HitagiText.icon(
+                'Volumes - ${releases.volumes}',
+                Icons.video_library,
+              )
+            : const SizedBox.shrink(),
         const SizedBox(height: 4),
         HitagiText.icon(
           'Próximo episódio - ${DateTimeUtil().formatUnixDateTime(releases.airingAt, 'dd-MM')}',
