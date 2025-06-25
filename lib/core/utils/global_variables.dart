4aba2899dc35d210db0ac7dc35655bcd63d12705
Gabriel Mello <gabrielrmello515@gmail.com>
2025-06-24 21:05:45 -0300

    feat: add user-related global variables for authentication state management

--- lib/core/utils/global_variables.dart
+++ lib/core/utils/global_variables.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:geekcontrol/view/services/profile/model/user_entity.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 
 GetIt di = GetIt.instance;
 
 class Globals extends ChangeNotifier {
+  static UserEntity? user;
+  static String? uid;
+  static bool isLoggedIn = false;
   void initializeLogger(Type caller) {
     Logger().t('Inicializando [$caller]...');
   }
