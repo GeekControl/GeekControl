3a25e726230b463dfb9185b51de8157f04cbd789
Gabriel Mello <gabrielrmello515@gmail.com>
2025-06-25 01:59:21 -0300

    feat: add repository registration in AnilistModules for improved dependency injection

--- lib/view/services/anilist/anilist_modules.dart
+++ lib/view/services/anilist/anilist_modules.dart
@@ -1,6 +1,7 @@
 import 'package:geekcontrol/core/utils/global_variables.dart';
 import 'package:geekcontrol/core/utils/module_factory.dart';
 import 'package:geekcontrol/view/services/anilist/controller/anilist_controller.dart';
+import 'package:geekcontrol/view/services/anilist/repository/anilist_repository.dart';
 
 class AnilistModules implements Module {
   @override
@@ -8,5 +9,7 @@ class AnilistModules implements Module {
     Globals().initializeLogger(runtimeType);
     //Controllers
     di.registerFactory(() => AnilistController());
+    //Repositories
+    di.registerFactory(() => AnilistRepository());
   }
 }
