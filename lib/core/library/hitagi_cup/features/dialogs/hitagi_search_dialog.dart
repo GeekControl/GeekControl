ed827de1148bd81dc9a9f33aee821e60aa395dc3
Gabriel Mello <gabrielrmello515@gmail.com>
2025-07-07 21:21:58 -0300

    feat: enhance HitagiSearchDialog UI with improved styling and interaction

--- lib/core/library/hitagi_cup/features/dialogs/hitagi_search_dialog.dart
+++ lib/core/library/hitagi_cup/features/dialogs/hitagi_search_dialog.dart
@@ -17,29 +17,87 @@ class HitagiSearchDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[200],
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.08),
+            blurRadius: 12,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
       child: TextField(
         controller: controller,
         onChanged: onChanged,
         onSubmitted: onSubmitted,
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.grey.shade800,
+        ),
         decoration: InputDecoration(
           hintText: hintText,
-          hintStyle: const TextStyle(color: Colors.grey),
-          isDense: true,
-          prefixIcon: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
+          hintStyle: TextStyle(
+            color: Colors.grey.shade500,
+            fontSize: 16,
           ),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.send),
-            onPressed: () {
-              if (onSubmitted != null) {
-                onSubmitted!(controller.text);
-              }
-            },
+          prefixIcon: Container(
+            margin: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Icon(
+              Icons.search,
+              color: Colors.grey.shade600,
+              size: 20,
+            ),
+          ),
+          suffixIcon: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            margin: const EdgeInsets.all(8),
+            child: Material(
+              color: Colors.blue.shade600,
+              borderRadius: BorderRadius.circular(12),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {
+                  if (onSubmitted != null) {
+                    onSubmitted!(controller.text);
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.arrow_forward_rounded,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
           ),
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(
+              color: Colors.blue.shade400,
+              width: 2,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.grey.shade50,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
           ),
         ),
       ),
