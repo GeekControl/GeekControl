0c0249d98142eb91a3808450a8a7a07f749b04aa
Gabriel Mello <gabrielrmello515@gmail.com>
2025-07-14 18:51:42 -0300

    feat: refactor HitagiBanner to StatefulWidget and enhance ArticleDetailsPage layout with improved loading indicator and UI elements

--- lib/core/library/hitagi_cup/features/carousel/hitagi_banner_carousel.dart
+++ lib/core/library/hitagi_cup/features/carousel/hitagi_banner_carousel.dart
@@ -4,7 +4,7 @@ import 'package:geekcontrol/core/library/hitagi_cup/features/images/hitagi_image
 import 'package:geekcontrol/core/library/hitagi_cup/features/text/hitagi_text.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class HitagiBanner extends StatelessWidget {
+class HitagiBanner extends StatefulWidget {
   final List<String> images;
   final String? title;
   final void Function(int index)? onTap;
@@ -17,60 +17,142 @@ class HitagiBanner extends StatelessWidget {
   });
 
   @override
+  State<HitagiBanner> createState() => _HitagiBannerState();
+}
+
+class _HitagiBannerState extends State<HitagiBanner> {
+  int currentIndex = 0;
+  late CarouselSliderController carouselController;
+
+  @override
+  void initState() {
+    super.initState();
+    carouselController = CarouselSliderController();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      options: CarouselOptions(
-        height: title != null ? 200 : 150,
-        aspectRatio: 16 / 9,
-        enlargeCenterPage: true,
-        autoPlay: images.length > 1,
-        autoPlayInterval: const Duration(seconds: 3),
-        autoPlayAnimationDuration: const Duration(seconds: 1),
-        enableInfiniteScroll: images.length > 1,
-        viewportFraction: images.length > 1 ? 0.8 : 1,
-      ),
-      itemCount: images.length,
-      itemBuilder: (context, index, realIndex) {
-        return GestureDetector(
-          onTap: () =>  onTap != null ? onTap!(index) : launchUrl(Uri.parse(images[index])),
-          child: title != null
-              ? Stack(
-                  children: [
-                    HitagiImages(
-                      image: images[index],
-                      width: double.infinity,
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      left: 0,
-                      right: 0,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 12,
-                        ),
+    return SizedBox(
+      height: widget.title != null ? 280 : 200,
+      child: Stack(
+        children: [
+          CarouselSlider.builder(
+            carouselController: carouselController,
+            options: CarouselOptions(
+              height: widget.title != null ? 280 : 200,
+              aspectRatio: 16 / 9,
+              enlargeCenterPage: false,
+              autoPlay: widget.images.length > 1,
+              autoPlayInterval: const Duration(seconds: 4),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              enableInfiniteScroll: widget.images.length > 1,
+              viewportFraction: 1.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            ),
+            itemCount: widget.images.length,
+            itemBuilder: (context, index, realIndex) {
+              return GestureDetector(
+                onTap: () => widget.onTap != null 
+                    ? widget.onTap!(index) 
+                    : launchUrl(Uri.parse(widget.images[index])),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      HitagiImages(
+                        image: widget.images[index],
+                        width: double.infinity,
+                      ),
+                      Container(
                         decoration: BoxDecoration(
-                          color: Colors.black.withValues(alpha: 0.5),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: HitagiText(
-                          text: title!,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                          color: Colors.white,
-                          typography: HitagiTypography.button,
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withValues(alpha: 0.3),
+                              Colors.black.withValues(alpha: 0.7),
+                            ],
+                            stops: const [0.0, 0.6, 1.0],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              : HitagiImages(
-                  image: images[index],
-                  width: double.infinity,
+                      if (widget.title != null)
+                        Positioned(
+                          bottom: 24,
+                          left: 24,
+                          right: 24,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              HitagiText(
+                                text: widget.title!,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                color: Colors.white,
+                                typography: HitagiTypography.title,
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
-        );
-      },
+              );
+            },
+          ),
+          if (widget.images.length > 1) ...[
+            Positioned(
+              bottom: 16,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: widget.images.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => carouselController.animateToPage(entry.key),
+                    child: Container(
+                      width: currentIndex == entry.key ? 24 : 8,
+                      height: 8,
+                      margin: const EdgeInsets.symmetric(horizontal: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: currentIndex == entry.key
+                            ? Colors.white
+                            : Colors.white.withValues(alpha: 0.4),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: HitagiText(
+                  text: '${currentIndex + 1}/${widget.images.length}',
+                  color: Colors.white,
+                  typography: HitagiTypography.small,
+                ),
+              ),
+            ),
+          ],
+        ],
+      ),
     );
   }
 }
--- lib/view/animes/articles/pages/article_details_page.dart
+++ lib/view/animes/articles/pages/article_details_page.dart
@@ -45,43 +45,202 @@ class _ArticleDetailsPageState extends State<ArticleDetailsPage> {
   Widget build(BuildContext context) {
     if (article.content.isEmpty) {
       return Scaffold(
-          body: const Center(
-        child: CircularProgressIndicator(),
-      ));
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-            child: HitagiText(
-          text: 'Detalhes da Notícia',
-          typography: HitagiTypography.title,
-        )),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            article.imagesPage != null
-                ? HitagiBanner(
-                    images: article.imagesPage!, title: article.title)
-                : HitagiBanner(
-                    images: [article.imageUrl ?? ''], title: article.title),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  HitagiText(
-                    text: 'Por ${article.author} | ${article.date}',
-                  ),
-                  const SizedBox(height: 16),
-                  HitagiText(text: article.content),
-                ],
+        backgroundColor: Colors.grey[50],
+        body: Center(
+          child: Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.purple[50],
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Center(
+              child: SizedBox(
+                width: 32,
+                height: 32,
+                child: CircularProgressIndicator(
+                  strokeWidth: 3,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.purple[600]!),
+                ),
               ),
             ),
-          ],
+          ),
         ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 250,
+            floating: false,
+            pinned: true,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.black.withValues(alpha: 0.2),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            actions: [
+              Container(
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.black.withValues(alpha: 0.2),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.share, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              background: article.imagesPage != null
+                  ? HitagiBanner(
+                      images: article.imagesPage!, 
+                      title: article.title,
+                    )
+                  : HitagiBanner(
+                      images: [article.imageUrl ?? ''], 
+                      title: article.title,
+                    ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    HitagiText(
+                      text: article.title,
+                      typography: HitagiTypography.title,
+                      color: Colors.grey[800],
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.purple[50],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.person_outline,
+                            size: 16,
+                            color: Colors.purple[600],
+                          ),
+                          const SizedBox(width: 8),
+                          HitagiText(
+                            text: article.author,
+                            typography: HitagiTypography.small,
+                            color: Colors.purple[600],
+                          ),
+                          const SizedBox(width: 16),
+                          Icon(
+                            Icons.access_time,
+                            size: 16,
+                            color: Colors.purple[600],
+                          ),
+                          const SizedBox(width: 8),
+                          HitagiText(
+                            text: article.date,
+                            typography: HitagiTypography.small,
+                            color: Colors.purple[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Container(
+                      width: double.infinity,
+                      height: 1,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.purple[200]!,
+                            Colors.purple[100]!,
+                            Colors.purple[200]!,
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    HitagiText(
+                      text: article.content,
+                      typography: HitagiTypography.body,
+                      color: Colors.grey[700],
+                    ),
+                    const SizedBox(height: 32),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[50],
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(color: Colors.grey[200]!),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.purple[100],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Icon(
+                              Icons.link,
+                              color: Colors.purple[600],
+                              size: 20,
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                HitagiText(
+                                  text: 'Fonte',
+                                  typography: HitagiTypography.small,
+                                  color: Colors.grey[500],
+                                ),
+                                const SizedBox(height: 2),
+                                HitagiText(
+                                  text: article.site,
+                                  typography: HitagiTypography.buttonNormal,
+                                  color: Colors.grey[700],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
